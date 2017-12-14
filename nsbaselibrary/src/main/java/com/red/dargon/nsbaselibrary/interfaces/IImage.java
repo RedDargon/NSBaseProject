2f52ce14fc1034dd538236aa0cbb50f2f28fed33
814624641@qq.com <814624641@qq.com>
2017-12-14 16:25:41 +0800

    cxl 引入vo的base接口

---
+++ nsbaselibrary/src/main/java/com/red/dargon/nsbaselibrary/interfaces/IImage.java
@@ -0,0 +1,13 @@
+package com.red.dargon.nsbaselibrary.interfaces;
+
+/**
+ * 所有的图片相关的类的接口。
+ */
+public interface IImage {
+    int getCropWidth();
+    int getCropHeight();
+    int getCropx();
+    int getCropy();
+    String getImgUrl();
+
+}
---
+++ nsbaselibrary/src/main/java/com/red/dargon/nsbaselibrary/interfaces/IVO.java
@@ -0,0 +1,11 @@
+package com.red.dargon.nsbaselibrary.interfaces;
+
+import java.io.Serializable;
+
+/**
+ * 所有javabean都需要实现这个接口
+ * Created by Dargon on 2017/6/21.
+ */
+
+public interface IVO extends Serializable {
+}
